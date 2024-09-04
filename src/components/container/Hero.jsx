2745926fef8611bea8e61f37cb7e5ddafcd805ee
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 17:01:23 +0100

    Create Hero.jsx

---
+++ src/components/container/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='relative w-full bg-black/80'>
+      <img src="" className='w-full inset-0' alt="" />
+      <div className="absolute w-full h-full">
+        <div className="flex flex-col items-center justify-center gap-2">
+            <span>Easy Ride by Easing Life</span>
+        </div>
+      </div>
+    </div>
+  )
+}
