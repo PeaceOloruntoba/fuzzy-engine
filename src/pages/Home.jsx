eb2d45c9a73efaac6f639b8013cb63ebfb215910
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 17:06:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/container/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full flex flex-col">
+      <Hero />
       <p>Home Page</p>
     </div>
-  )
+  );
 }
