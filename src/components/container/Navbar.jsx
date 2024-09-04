e8cec807469c5bb35e41012d0673db7bb3b12af1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 16:15:02 +0100

    soft

--- src/components/container/Navbar.jsx
+++ src/components/container/Navbar.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="w-full text-black/70 bg-white/80 text-xl flex items-center justify-between">
+    <div className="w-full text-black/90 text-xl flex items-center justify-between font-semibold py-2 px-6">
       <div className="flex items-center justify-center">
         <img src="" alt="" />
       </div>
-      <div className="md:flex items-center justify-center hidden">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Services</a>
-        <a href="">Faq</a>
-        <a href="">Testimonials</a>
-        <a href="">Contact Us</a>
+      <div className="md:flex items-center justify-center hidden gap-2">
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Home</a>
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">About</a>
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Services</a>
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Faq</a>
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Testimonials</a>
+        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Contact Us</a>
       </div>
     </div>
   );
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/container/Header";
+import Navbar from "../components/container/Navbar";
 
 export default function RootLayout() {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Header />
+      <div className="sticky z-50">
+        <Navbar />
+      </div>
       <Outlet />
     </div>
   );
