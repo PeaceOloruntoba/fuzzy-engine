bd7e8c102e34136c05ebf36b3209177eb53b513c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 09:11:41 +0100

    Update RootLayout.jsx

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex flex-col'>
+      <Outlet />
     </div>
   )
 }
