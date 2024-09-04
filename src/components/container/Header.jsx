fb78e59146616c74318f3aa77e2c499975763c9e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 16:00:57 +0100

    Update Header.jsx

--- src/components/container/Header.jsx
+++ src/components/container/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { FaMailchimp } from "react-icons/fa6";
 
 export default function Header() {
   return (
-    <div className='w-full bg-yellow-500 text-white font-semibold md:text-xl text-sm'>
-        <div className="flex gap-4 items-cente justify-center">
-            <a href="mailto:"></a>
-            </div>      
+    <div className="w-full bg-yellow-500 text-white font-semibold md:text-xl text-sm">
+      <div className="flex gap-4 items-cente justify-center">
+        <span className="flex gap-2 items-center justify-center">
+          <FaMailchimp />
+          <a href="mailto:easinglife95@gmail.com">easinglife95@gmail.com</a>
+        </span>
+      </div>
     </div>
-  )
+  );
 }
