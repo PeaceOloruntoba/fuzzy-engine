fdafb69f02b44c4932547298e931200a7c674128
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-04 15:59:35 +0100

    Create Header.jsx

---
+++ src/components/container/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full bg-yellow-500 text-white font-semibold md:text-xl text-sm'>
+        <div className="flex gap-4 items-cente justify-center">
+            <a href="mailto:"></a>
+            </div>      
+    </div>
+  )
+}
