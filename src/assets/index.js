2ccbf4f5202dce907e1fde5821b09c02d52742c1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 05:06:51 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import herobg from "./images/herobg.jpg"
 import logo from "./images/logo.jpg"
 import bookTaxi from "./images/bookTaxi.jpg"
 import hireDriver from "./images/hireDriver.jpg"
-import playStore from "./images/playStore.png"
-import appleStore from "./images/appleStore.png"
+import playStore from "./images/playstore.png"
+import appleStore from "./images/applestore.png"
 
 export default {herobg, logo, bookTaxi, hireDriver, playStore, appleStore}
