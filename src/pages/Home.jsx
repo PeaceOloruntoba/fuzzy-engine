3f4e42dbd07a2c0b2b33b65b63bd2c2912e4e549
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 04:05:44 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/container/Hero";
 import Services from "../components/container/Services";
 import Whys from "../components/container/Whys";
 import FaqsAll from "../components/container/FaqsAll";
+import Testimony from "../components/container/Testimony";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Services />
       <Whys />
       <FaqsAll />
+      <Testimony />
       <p>Home Page</p>
     </div>
   );
