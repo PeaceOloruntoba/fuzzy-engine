9bd98e186d39e8457ba8187744658c010c5828ca
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 03:12:39 +0100

    Whys

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/container/Hero";
 import Services from "../components/container/Services";
+import Whys from "../components/container/Whys";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col">
       <Hero />
       <Services />
+      <Whys />
       <p>Home Page</p>
     </div>
   );
