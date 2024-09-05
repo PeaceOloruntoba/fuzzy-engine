7ff516bb701e5438f4f2a29f80b7eeccd90bbd91
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 02:50:49 +0100

    Update Services.jsx

--- src/components/container/Services.jsx
+++ src/components/container/Services.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Services() {
   return (
-    <div>
-      
+    <div className="flex flex-col w-full bg-black/80 text-white">
+      <span>Services</span>
+      <div className="grid grid-cols-2 items-center justify-center">
+        <div className="flex p-4 border border-yellow-600">
+          <img src="" alt="" />
+        </div>
+        <div className="flex">
+          <span></span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
