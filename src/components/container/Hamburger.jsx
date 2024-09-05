b3a7af7a054986bd7201792a1c8e6b88fd061534
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 04:35:19 +0100

    mmm

--- src/components/container/Hamburger.jsx
+++ src/components/container/Hamburger.jsx
@@ -1,45 +1,59 @@
 import React, { useState } from "react";
 import { CgMenuRight, CgCloseR } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 export default function Hamburger() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div className="">
+    <div>
       <button
         className="text-gray-500 focus:outline-none focus:text-gray-600"
         onClick={toggleMenu}
       >
-        <CgMenuRight className="text-3xl font-semibold" />
+        {isOpen ? (
+          <CgCloseR className="text-3xl" />
+        ) : (
+          <CgMenuRight className="text-3xl" />
+        )}
       </button>
 
       {isOpen && (
-        <div className="fixed top-0 left-0 bottom-0 z-10 bg-opacity-90 w-screen h-screen bg-black/80 text-white font-semibold text-[22px] shadow-lg">
-          <button onClick={toggleMenu} className="absolute top-5 right-5">
-            <CgCloseR className="text-2xl text-white/70" />
-          </button>
-
-          <div className="flex flex-col gap-16 items-start h-screen p-4 pt-[4em]">
+        <div className="fixed top-0 left-0 bottom-0 z-10 bg-opacity-90 w-screen h-screen bg-black/80 text-white font-semibold text-lg shadow-lg">
+          <div className="flex flex-col gap-8 items-start h-screen p-4 pt-[4em]">
             <ul className="flex flex-col gap-4">
               <li>
-                <a href="">Home</a>
+                <Link to="/" onClick={() => setIsOpen(false)}>
+                  Home
+                </Link>
               </li>
               <li>
-                <a href="">About</a>
+                <Link to="/about" onClick={() => setIsOpen(false)}>
+                  About
+                </Link>
               </li>
               <li>
-                <a href="">Services</a>
+                <Link to="/services" onClick={() => setIsOpen(false)}>
+                  Services
+                </Link>
               </li>
               <li>
-                <a href="">Faqs</a>
+                <Link to="/faqs" onClick={() => setIsOpen(false)}>
+                  Faq
+                </Link>
               </li>
               <li>
-                <a href="">Testimonials</a>
+                <Link to="/testimonials" onClick={() => setIsOpen(false)}>
+                  Testimonials
+                </Link>
               </li>
               <li>
-                <a href="">Contact Us</a>
+                <Link to="/contact" onClick={() => setIsOpen(false)}>
+                  Contact Us
+                </Link>
               </li>
             </ul>
           </div>
--- src/components/container/Navbar.jsx
+++ src/components/container/Navbar.jsx
@@ -1,19 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Hamburger from "./Hamburger";
+import images from "../../assets";
+
+const logo = images?.logo;
 
 export default function Navbar() {
   return (
-    <div className="w-full text-black/90 text-xl flex items-center justify-between font-semibold py-2 px-6">
+    <div className="w-full text-black/90 text-xl flex items-center justify-between font-semibold px-6 bg-white shadow-md">
       <div className="flex items-center justify-center">
-        <img src="" alt="" />
+        <img src={logo} alt="Logo" className="h-20" />{" "}
       </div>
       <div className="md:flex items-center justify-center hidden gap-2">
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Home</a>
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">About</a>
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Services</a>
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Faq</a>
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Testimonials</a>
-        <a href="" className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1">Contact Us</a>
+        <Link
+          to="/"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          About
+        </Link>
+        <Link
+          to="/services"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          Services
+        </Link>
+        <Link
+          to="/faqs"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          Faq
+        </Link>
+        <Link
+          to="/testimonials"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          Testimonials
+        </Link>
+        <Link
+          to="/contact"
+          className="hover:bg-gray-300 px-3 rounded-md transition-all duration-500 py-1"
+        >
+          Contact Us
+        </Link>
+      </div>
+      <div className="md:hidden">
+        <Hamburger />
       </div>
     </div>
   );
