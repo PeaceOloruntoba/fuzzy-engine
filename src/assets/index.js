518a04bb2604fea1e14d723f83d40f3b7639ff33
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 02:59:15 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import herobg from "./images/herobg.jpg"
 import logo from "./images/logo.jpg"
+import bookTaxi from "./images/bookTaxi.jpg"
+import hireDriver from "./images/hireDriver.jpg"
 
-export default {herobg, logo}
+export default {herobg, logo, bookTaxi, hireDriver}
